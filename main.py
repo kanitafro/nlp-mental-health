8b33c68cbbe01f6c92983e6cbc49b07d9b36aca9
Kanita Tafro <tafro.kanita@gmail.com>
2025-11-24 22:00:38 +0100

    Debug after 1st run
    Update main so it doesn't run the cleaning pipeline

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import pandas as pd
+import numpy as np
 
-from scripts.clean_dataset import run_cleaning_pipeline
 from data.lexicon.build_lexicon import build_and_save_lexicon
 from preprocessing.tokenizer import fit_tfidf_vectorizer
 
 
 def main():
     build_and_save_lexicon()
-    print("=== Clean 6 labels dataset ===")
-    run_cleaning_pipeline()
 
     df = pd.read_csv("data/processed/dataset_6labels_clean.csv")
     texts = df["clean_text_ml"].tolist()
 
-
+    texts = ["" if text is None or (isinstance(text, float) and np.isnan(text)) else str(text) for text in texts]
     vectorizer, X = fit_tfidf_vectorizer(
         texts,
         mode="ml",
         use_ner_tags=True,
         max_features=50000,
-        min_df=3,
+        min_df=2,
         max_df=0.9,
         save_path="models/tfidf.joblib"
     )
