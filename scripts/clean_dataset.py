a28699b9d6679ec3b59622c475fcd27466eb6b2c
Kanita Tafro <tafro.kanita@gmail.com>
2025-11-24 21:57:12 +0100

    Debug after 1st run
    Update cleaning pipeline and make sure it's not called from main.py but it requires to be run separately (since dataset cleaning is done only once)

--- scripts/clean_dataset.py
+++ scripts/clean_dataset.py
@@ -6,26 +6,40 @@ from preprocessing.map_emotions import REVERSE_MAP, map_emotions_df_column
 tqdm.pandas()
 
 def run_cleaning_pipeline():
-    print("Loading dataset (with 6 emotions)...")
+    print("=== Loading dataset (6 emotions) ===")
 
     df = pd.read_csv("data/raw/dataset_6labels.csv")
 
+    print(f"Loaded dataset_6labels.csv with {len(df)} rows")
+
     # rename columns to be 'text' and 'label'
     df = df.rename(columns={'sentence': 'text', 'emotion': 'label'})
+    print("Column names after renaming:", end=" ")
+    for col in df.columns:
+        print(col, end=", ")
+    # rename label 'sad' to 'sadness'
+    df['label'] = df['label'].replace('sad', 'sadness')
+    print("Labels: ", df['label'].unique())
 
+    print("=== Clean 6 labels dataset ===")
+    run_cleaning_pipeline()
     # Apply all cleaning versions
+    print("Cleaning text for ML mode without NER tags:")
     df["clean_text_ml"] = df["text"].progress_apply(
         lambda x: clean_text(str(x), mode="ml")
     )
 
+    print("Cleaning text for ML mode with NER tags:")
     df["clean_text_ml_ner"] = df["text"].progress_apply(
         lambda x: clean_text(str(x), mode="ml", use_ner_tags=True)
     )
 
+    print("Cleaning text for transformer mode without NER tags:")
     df["clean_text_transf"] = df["text"].progress_apply(
         lambda x: clean_text(str(x), mode="transformer")
     )
 
+    print("Cleaning text for transformer mode with NER tags:")
     df["clean_text_transf_ner"] = df["text"].progress_apply(
         lambda x: clean_text(str(x), mode="transformer", use_ner_tags=True)
     )
