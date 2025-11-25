d857600fda125a3ee71f315ab0235261baf0bccb
Kanita Tafro <tafro.kanita@gmail.com>
2025-11-25 13:32:14 +0100

    Import train_logreg_v1 and update main

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 
 from preprocessing.tokenizer import fit_tfidf_vectorizer
+from models.train_logreg_v1 import train_logreg_v1
 
 def main():
     df = pd.read_csv("data/processed/dataset_6labels_clean.csv")
+    """
+    # This will be run in the models/ files
     texts = df["clean_text_ml"].tolist()
 
     texts = ["" if text is None or (isinstance(text, float) and np.isnan(text)) else str(text) for text in texts]
@@ -20,6 +23,8 @@ def main():
 
     # ready for ML models
     print(X.shape)
+    """
+    # train_logreg_v1()
 
 if __name__ == "__main__":
     main()
