7f9cbdd7fd2aa12de2e9678eed1f1b26adf698e7
Kanita Tafro <tafro.kanita@gmail.com>
2026-05-15 12:46:16 +0200

    Create scrip for merging raw data (6 and 7 labels)

--- scripts/merge_to_6labels_7labels.py
+++ scripts/merge_to_6labels_7labels.py
@@ -1,12 +1,25 @@
-
 import pandas as pd
-
 
 path_raw_6labels = "data/raw/dataset_6labels.csv"
 path_raw_love_surprise = "data/raw/love_surprise_bonus.csv"
 path_raw_disgust = "data/raw/disgust_bonus.csv"
 
-dir_7labels = "data/raw"
+dir_raw = "data/raw"
+
+def merge_to_6labels():
+    print("Loading full dataset (6 labels)...")
+    df = pd.read_csv(path_raw_6labels)
+
+    print("Loading dataset (love & surprise)...")
+    df_love_surprise = pd.read_csv(path_raw_love_surprise)
+
+    df_merged = pd.concat([df, df_love_surprise], ignore_index=True)
+    path_6labels_more = f"{dir_raw}/dataset_6labels_more.csv"
+    df_merged.to_csv(path_6labels_more, index=False)
+
+    print("\nMerged dataset size: ", len(df_merged))
+    print(f"Value counts in 'label' column: {df_merged['label'].value_counts()}")
+    print(f"Saved merged 6-emotions dataset -> {path_6labels_more}")
 
 def merge_to_7labels():
     print("Loading full dataset (6 labels)...")
@@ -19,11 +32,13 @@ def merge_to_7labels():
     df_disgust = pd.read_csv(path_raw_disgust)
 
     df_merged = pd.concat([df, df_love_surprise, df_disgust], ignore_index=True)
-    path_7labels = f"{dir_7labels}/dataset_7labels.csv"
+    path_7labels = f"{dir_raw}/dataset_7labels.csv"
     df_merged.to_csv(path_7labels, index=False)
+
     print("\nMerged dataset size: ", len(df_merged))
     print(f"Value counts in 'label' column: {df_merged['label'].value_counts()}")
     print(f"Saved merged 7-emotions dataset -> {path_7labels}")
 
 if __name__ == "__main__":
+    merge_to_6labels()
     merge_to_7labels()
